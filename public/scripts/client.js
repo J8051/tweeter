b47483f30a8013f5b51202075fbbf34701c735c5
J8051 <nathaniel.jessica@yahoo.ca>
2023-01-24 17:04:11 -0500

    included ajax get request to render tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,91 +3,58 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function () {
-  const createTweetElement = function(tweetObj) { 
-    const $tweet = $(`
-    <article class="tweet">
-    <header class="tweet-header">
-    
-      <div>
-        <img src="${tweetObj["user"].avatars}" alt="icon of user's display photo">
-        <span>${tweetObj["user"].name}</span>
-      </div>
-    
-      <span> ${tweetObj["user"].handle}</span>
-    </header>
-    
-    <div class="tweet-body">
-    <p> ${tweetObj["content"].text}</p>
+const createTweetElement = function(tweetObj) {
+  const $tweet = $(`
+  <article class="tweet">
+  <header class="tweet-header">
+  
+    <div>
+      <img src="${tweetObj["user"].avatars}" alt="icon of user's display photo">
+      <span>${tweetObj["user"].name}</span>
     </div>
-    
-    <footer class="tweet-footer">
-      <p>${tweetObj.created_at}</p>
-    
-      <div>
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    
-    </footer>
-    
-    </article>
   
-    `);  
-    return $tweet; 
+    <span> ${tweetObj["user"].handle}</span>
+  </header>
+  
+  <div class="tweet-body">
+  <p> ${tweetObj["content"].text}</p>
+  </div>
+  
+  <footer class="tweet-footer">
+    <p>${tweetObj.created_at}</p>
+  
+    <div>
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  
+  </footer>
+  
+  </article>
+
+  `);
+  return $tweet;
+};
+
+const renderTweets = function($tweets) {
+
+  for (let tweet of $tweets) {
+    let $tweetElement = createTweetElement(tweet);
+    $('#tweet-container').prepend($tweetElement);
+    // console.log($tweetElement); 
+  }
+
+};
+
+$(document).ready(function() {
+
+  const loadTweets = function() {
+    $.get("http://localhost:8080/tweets", (data) => {
+      renderTweets(data);
+    });
+
+
   };
-  
-  const objTweet = {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-          "handle": "@SirIsaac"
-        },
-      "content": {
-          "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-      "created_at": 1461116232227
-  };
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-  
-  const $tweets = createTweetElement(objTweet);
-
-  const renderTweets = function($tweets) {
-
-    for (let tweet of $tweets) { 
-      let $tweetElement = createTweetElement(tweet)
-      $('#tweet-container').append($tweetElement);
-      // console.log($tweetElement); 
-    }
-
-  }; 
-
-  renderTweets(data);
-
+  loadTweets();
 }); 
--- public/scripts/events.js
+++ public/scripts/events.js
@@ -3,8 +3,20 @@ $(document).ready(function() {
   $(".new-tweet form").submit(function(event) {
     event.preventDefault(); 
 
-    $.post("http://localhost:8080/tweets", $(this).serialize()); 
-    // console.log($(this).serialize()); 
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      type: "application/json",
+      data: $(this).serialize(),
+      success: function() {
+        $("textarea").val("");
+        $.get("http://localhost:8080/tweets", data => {
+          const newTweet = [data.slice(-1).pop()];
+          renderTweets(newTweet);
+        });
+      }
+    }); 
+
   });
 
 
