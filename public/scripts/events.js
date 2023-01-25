64c70900054b284633fb9f49f2e6aae6f60f6d3c
J8051 <nathaniel.jessica@yahoo.ca>
2023-01-25 10:13:54 -0500

    form validation included to ensure appropriate character length for tweets

--- public/scripts/events.js
+++ public/scripts/events.js
@@ -3,20 +3,25 @@ $(document).ready(function() {
   $(".new-tweet form").submit(function(event) {
     event.preventDefault();
 
-    $.ajax({
-      method: "POST",
-      url: "/tweets",
-      type: "application/json",
-      data: $(this).serialize(),
-      success: function() {
-        $("textarea").val("");
-        $.get("http://localhost:8080/tweets", data => {
-          const newTweet = [data.slice(-1).pop()];
-          renderTweets(newTweet);
-        });
-      }
-    });
-
+    if (!$("textarea").val()) {
+      alert("Oops! Your tweet is empty. Please add some text");
+    } else if ($("textarea").val().length > 140) {
+      alert("Oops! Your tweet is too long. The character limit is 140");
+    } else {
+      $.ajax({
+        method: "POST",
+        url: "/tweets",
+        type: "application/json",
+        data: $(this).serialize(),
+        success: function() {
+          $("textarea").val("");
+          $.get("http://localhost:8080/tweets", data => {
+            const newTweet = [data.slice(-1).pop()];
+            renderTweets(newTweet);
+          });
+        }
+      });
+    }
   });
 
 
